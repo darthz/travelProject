91ce235642e925cca05576715b89762bb80171c7
Bruno Lima <brunolima@Brunos-MacBook-Pro.local>
2023-10-29 13:46:29 -0300

    Travel Project

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  const handleAddItems = (item) => {
+    setItems((items) => [...items, item]);
+  };
+
+  const handleDeleteItem = (id) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
+
+  const handleToggleItem = (id) => {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  };
+
+  const clearList = () => {
+    const confirmed = window.confirm(
+      "Are you sure you want do delete everything?"
+    );
+
+    if (confirmed) setItems([]);
+  };
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={clearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+};
+
+export default App;
